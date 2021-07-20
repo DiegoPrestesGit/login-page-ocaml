1d3779e5a59ecd71ace21f0ab0018d5cd35c5175
Diego Prestes <diegoprestes.git@hotmail.com>
2021-07-19 23:47:04 -0300

    feat: add lists

--- chapter4.ml
+++ chapter4.ml
@@ -1,2 +1,27 @@
+(* LISTS *)
 open Format
 (* TODO *)
+let isnil list =
+  match list with
+    [] -> true
+  | _ -> false;;
+
+let isIt = isnil [];;
+print_bool isIt;;
+printf "\n";;
+
+let rec length list =
+  match list with
+    [] -> 0
+  | h::t -> 1 + length t;;
+
+print_int (length [1;2;3;4;5]);;
+printf "\n";;
+
+let rec sum list =
+  match list with
+    [] -> 0
+  | h::t -> h + sum t;;
+
+print_int (sum [5;5;5;5]);;
+printf "\n";;
