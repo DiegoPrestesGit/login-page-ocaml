c8f60f08e4a23ff38334e8a9c5b2b35b3d2bc4b4
Diego Prestes <diegoprestes.git@hotmail.com>
2021-07-31 22:38:04 -0300

    feat: add new list functions

--- chapter5.ml
+++ chapter5.ml
@@ -1,3 +1,12 @@
+open Format;;
+
+let functions_list = [1; 10; 24; 11; 2; 63; 5; 34; 6; 10];;
+
+let rec print_list list =
+  match list with
+    [] -> printf "\n"; []
+  | h::t -> print_int h; print_list t;;
+
 let rec insert x l =
   match l with
     [] -> [x]
@@ -11,3 +20,4 @@ let rec sort list =
     [] -> []
   | h::t -> insert h (sort t);;
 
+print_list (sort functions_list);;
